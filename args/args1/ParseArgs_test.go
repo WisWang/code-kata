9fe3c6c1845c5d7ff2f45b0b4168a9f2adfa7e2f
hongzhi.wang <hongzhi.wang@moji.com>
2019-04-23 14:17:54 +0800

    add another unit test

--- args/args1/ParseArgs_test.go
+++ args/args1/ParseArgs_test.go
@@ -1,14 +1,14 @@
 package args1
 
 import (
-	"testing"
+	"fmt"
 	"reflect"
-
+	"testing"
 )
 
 func TestGetArgs(t *testing.T) {
-	OsArgs := []string{"par.go","-d", "80", "-m","helo"}
-	expectArgs := []string{"-d", "80", "-m","helo"}
+	OsArgs := []string{"par.go", "-d", "80", "-m", "helo"}
+	expectArgs := []string{"-d", "80", "-m", "helo"}
 	realArgs := GetArgs(OsArgs)
 	if !reflect.DeepEqual(expectArgs, realArgs) {
 		t.Error("get args failed")
@@ -16,80 +16,94 @@ func TestGetArgs(t *testing.T) {
 
 }
 
-func TestMatchArg(t *testing.T){
+func TestMatchArg(t *testing.T) {
 	ShouldMatchArg := []string{"-h", "-H"}
-	ShouldNotMatchArg := []string{"-5","A","-h3"}
+	ShouldNotMatchArg := []string{"-5", "A", "-h3"}
 	var arg string
-	for _,arg = range ShouldMatchArg{
-		if !MatchArg(arg){
+	for _, arg = range ShouldMatchArg {
+		if !MatchArg(arg) {
 			t.Errorf("%s should match\n", arg)
 		}
 	}
-	for _,arg = range ShouldNotMatchArg{
-		if MatchArg(arg){
+	for _, arg = range ShouldNotMatchArg {
+		if MatchArg(arg) {
 			t.Errorf("%s should not match\n", arg)
 		}
 	}
 }
 
 func TestMatchSchema(t *testing.T) {
-	ShouldMatchSchemas := []string{"-5,-3", "-4","false,node"}
-	ShouldNotMatchSchemas := []string{"-d","-h", "-node"}
+	ShouldMatchSchemas := []string{"-5,-3", "-4", "false,node"}
+	ShouldNotMatchSchemas := []string{"-d", "-h", "-node"}
 	var arg string
-	for _,arg = range ShouldMatchSchemas{
-		if !MatchSchema(arg){
+	for _, arg = range ShouldMatchSchemas {
+		if !MatchSchema(arg) {
 			t.Errorf("%s should match\n", arg)
 		}
 	}
-	for _,arg = range ShouldNotMatchSchemas{
-		if MatchSchema(arg){
+	for _, arg = range ShouldNotMatchSchemas {
+		if MatchSchema(arg) {
 			t.Errorf("%s should not match\n", arg)
 		}
 	}
 }
 
-func TestIterateArgs(t *testing.T)  {
+func TestIterateArgs(t *testing.T) {
 	RealArgs := []string{"-l", "-p", "8080", "-d", "/opt/logs"}
 	expectMap := map[string]string{
-		"l" : "",
-		"p" : "8080",
-		"d" : "/opt/logs",
+		"l": "",
+		"p": "8080",
+		"d": "/opt/logs",
 	}
-	if ArgsMap ,err:= IterateArgs(RealArgs);err == ""{
-		if !reflect.DeepEqual(ArgsMap, expectMap){
+	if ArgsMap, err := IterateArgs(RealArgs); err == "" {
+		if !reflect.DeepEqual(ArgsMap, expectMap) {
 			t.Errorf("ArgsMap: %s is not equal expectMap: %s\n", ArgsMap, expectMap)
 		}
 	}
 }
 
-func TestCommandP(t *testing.T)  {
-	errorSchemas := []string{"-12","nd","a2"}
-	rightSchemas := []string{"80","8080"}
-	for _, schema := range errorSchemas{
-		if err := CommandP(schema); err == ""{
+func TestCommandP(t *testing.T) {
+	errorSchemas := []string{"-12", "nd", "a2"}
+	rightSchemas := []string{"80", "8080"}
+	for _, schema := range errorSchemas {
+		if err := CommandP(schema); err == "" {
 			t.Errorf("schema: %s is not a valid schema for function p", schema)
 		}
 	}
-	for _, schema := range rightSchemas{
-		if err := CommandP(schema); err != ""{
+	for _, schema := range rightSchemas {
+		if err := CommandP(schema); err != "" {
 			t.Errorf("schema: %s is a valid schema for function p", schema)
 		}
 	}
 }
 
-
-func TestCommandL(t *testing.T)  {
-	errorSchemas := []string{"-12","nd","a2"}
-	rightSchemas := []string{"true","false",""}
-	for _, schema := range errorSchemas{
-		if err := CommandL(schema); err == ""{
+func TestCommandL(t *testing.T) {
+	errorSchemas := []string{"-12", "nd", "a2"}
+	rightSchemas := []string{"true", "false", ""}
+	for _, schema := range errorSchemas {
+		if err := CommandL(schema); err == "" {
 			t.Errorf("schema: %s is not a valid schema for function p", schema)
 		}
 	}
-	for _, schema := range rightSchemas{
-		if err := CommandL(schema); err != ""{
+	for _, schema := range rightSchemas {
+		if err := CommandL(schema); err != "" {
 			t.Errorf("schema: %s is a valid schema for function p", schema)
 		}
 	}
 }
 
+func TestProcessMap(t *testing.T) {
+	testCases := []map[string]string{
+		{
+			"l": "",
+			"p": "8080",
+			"d": "/opt/logs",
+		},
+	}
+	for _, testCase := range testCases {
+		if err := ProcessMap(testCase); err != "" {
+			fmt.Println(err)
+			t.Errorf("map: %s ProcessMap error", testCase)
+		}
+	}
+}
--- args/args1/parseArgs.go
+++ args/args1/parseArgs.go
@@ -3,93 +3,88 @@ package args1
 import (
 	"fmt"
 	"regexp"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type Args struct {
 	name rune
-
 }
 
+const HelpMessage = "help message"
 
-const HelpMessage  = "help message"
-
-func GetArgs(OSArgs []string) (args []string)   {
+func GetArgs(OSArgs []string) (args []string) {
 	args = OSArgs[1:]
 	return
 }
 
-func PrintHelpMessage()  {
-	fmt.Println(HelpMessage)
-}
-
 func MatchArg(arg string) bool {
-	if match,_ := regexp.MatchString(`-([a-zA-Z]$)`,arg); match{
+	if match, _ := regexp.MatchString(`-([a-zA-Z]$)`, arg); match {
 		return true
 	}
 	return false
 }
 
 func MatchSchema(arg string) bool {
-	if match,_ := regexp.MatchString(`^-[0-9]`,arg); match{
+	if match, _ := regexp.MatchString(`^-[0-9]`, arg); match {
 		return true
 	}
-	if match,_ := regexp.MatchString(`^/`,arg); match{
+	if match, _ := regexp.MatchString(`^/`, arg); match {
 		return true
 	}
-	if match,_ := regexp.MatchString(`^\W`,arg); match{
+	if match, _ := regexp.MatchString(`^\W`, arg); match {
 		return false
 	}
 	return true
 }
 
-func IterateArgs( args []string) ( ArgsMap  map[string]string, err string)  {
+func IterateArgs(args []string) (ArgsMap map[string]string, err string) {
 	index := 0
 	ArgsMap = make(map[string]string)
-	for(index < len(args)){
+	for index < len(args) {
 		if MatchArg(args[index]) {
-			if index+1 == len(args){
-				ArgsMap[strings.Replace(args[index],"-","",-1)] = ""
+			if index+1 == len(args) {
+				ArgsMap[strings.Replace(args[index], "-", "", -1)] = ""
 				break
 			}
 			if MatchSchema(args[index+1]) {
-				ArgsMap[strings.Replace(args[index],"-","",-1)] = args[index+1]
-				index ++
-			}else {
-				ArgsMap[strings.Replace(args[index],"-","",-1)] = ""
+				ArgsMap[strings.Replace(args[index], "-", "", -1)] = args[index+1]
+				index++
+			} else {
+				ArgsMap[strings.Replace(args[index], "-", "", -1)] = ""
 			}
 		}
-		index ++
+		index++
 	}
 	return ArgsMap, ""
 }
 
-func ProcessMap(ArgsMap  map[string]string) (err string) {
-	for arg,schema:= range ArgsMap{
+func ProcessMap(ArgsMap map[string]string) (err string) {
+	err = ""
+	for arg, schema := range ArgsMap {
 		switch arg {
-		case "h":
-			CommandD(schema)
+		case "d":
+			err = CommandD(schema)
 		case "p":
-			CommandP(schema)
+			err = CommandP(schema)
 		case "l":
-			CommandL(schema)
+			err = CommandL(schema)
 		default:
 			err = fmt.Sprintf("not support flags: %s", arg)
+		}
+		if err != "" {
 			return
 		}
 	}
-	return ""
+	return
 }
-
-
 
 func CommandD(schema string) (commandErr string) {
 	fmt.Printf("d function process %s\n", schema)
 	return ""
 }
 func CommandP(schema string) (commandErr string) {
-	portnumber,err := strconv.Atoi(schema)
+	portnumber, err := strconv.Atoi(schema)
 	if err != nil || portnumber < 0 {
 		commandErr = "p function only support positive int"
 		return
@@ -99,20 +94,14 @@ func CommandP(schema string) (commandErr string) {
 }
 
 func CommandL(schema string) (commandErr string) {
-	if schema == ""{
+	if schema == "" {
 		schema = "true"
 	}
-	logging,err := strconv.ParseBool(schema)
+	logging, err := strconv.ParseBool(schema)
 	if err != nil {
 		commandErr = "CommandL function only support bool"
 		return
 	}
 	fmt.Printf("l function process %t\n", logging)
 	return ""
-}
-
-func main()  {
-	fmt.Println("https://golang.org/pkg/regexp/")
-	fmt.Println("d")
-
 }
